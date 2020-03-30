b646919119ca9092707bcdeea2191796ee377ae3
Cristian Pascu <30505787+andrei-pascu@users.noreply.github.com>
2020-03-30 13:31:58 +0100

    Initial Mocks

---
+++ src/api/data_streaming.js
@@ -0,0 +1,25 @@
+
+
+
+  // setInterval(() => {
+  //   gets buffer_data
+    //   iterates through buffer_data
+        //   asks, 
+        //   is it a new timestamp?
+          //   if yes
+            //   push to LOCAL DATA STREAMING
+    
+  //   it can take 4 seconds
+      //   to get ONLY2 new (every second) timestamps
+  //   next 2s, youll get 6 new timestamps
+      //   => buffer will be filled
+  // }, 1000);
+
+
+
+
+  // GENERATE DATA 
+    // RANDOM add/remove PROCESSES
+    // RANDOM NR'S 9999 MOCK REAL-TIME
+        // ----> LOW USAGE NR'S (DONT EXCEED 100% lul)
+    // RANDOM DELAY 
