b850fb3fcf40c47e349f975c28c78ff4b43d3c5a
Cristian Pascu <30505787+andrei-pascu@users.noreply.github.com>
2020-03-30 22:48:47 +0100

    Working constructor based stream

--- src/api/data_stream.js
+++ src/api/data_stream.js
@@ -1,17 +1,6 @@
-// import mock_data_generator from '../mocked_datacenter/mock_data_generator';
 import mock_init_data_generator from '../mocked_datacenter/mock_init_data_generator';
 import mock_new_data_generator from '../mocked_datacenter/mock_new_data_generator';
 
-// console.log(get_data["history"](30)[0]["timestamp"])
-// setInterval(() => {
-//     console.log(get_data["new"](10)[0]["timestamp"])
-// }, 2000);
-// // console.log(mock_data_generator(30, true));
-
-
-// var buffer = get_data[0];
-// console.log('buffer', buffer[0]["timestamp"])
-// var new_data;
 
 var init_data = mock_init_data_generator(30);
 var buffer = init_data;
@@ -21,55 +10,33 @@ var new_entries = 0;
 
 
 function data_stream_update() {
-    new_data = mock_new_data_generator(10);
-    // tentative de dynamic stuff
-    // gen unshift si pop cate sunt noi
-
-    // for (let i = 0; i < buffer.length; i++) {
-    //     for (let j = 0; j < new_data.length; j++) {
-    //         if(buffer[i]["timestamp"] === new_data[j]["timestamp"]) {
-    //             new_entries = j-i;
-    //         } 
-    //     }
-    // }
     
+    new_data = mock_new_data_generator(10);
 
+    for (let i = 0; i < buffer.length; i++) {
+        for (let j = 0; j < new_data.length; j++) {
+            if(buffer[i]["timestamp"] === new_data[j]["timestamp"]) {
+                new_entries = j-i;
+            } 
+        }
+    }
 
-    // buffer.unshift(new_data[1], new_data[0])
-    // buffer = buffer.unshift()
+    console.log(new_entries)
+    for (let k = new_entries -1; k >= 0; k--) {
 
-        // ushift asta
-        // nu mege ok, gen face
-        // nd1, nd0, old, old, old, old
-        // nd1, nd0, nd1, nd0, old, old
-        // gen face update si la the last unshifts..
-                // => cre ca e de la time function ala tho
-                // pare ca face o referinta la functia aia in loc de fking una noua
-            // buffer.unshift(new_data[1])
-            // buffer.pop();
-            // buffer.unshift(new_data[0])
-            // buffer.pop();
-
-
-
-        buffer.unshift(new_data[1]['timestamp'])
+        buffer.unshift(new_data[k])
         buffer.pop();
-        buffer.unshift(new_data[0]['timestamp'])
-        buffer.pop();
-
-
-
-        console.log('newly added', buffer)
-
-        console.log('newly added', buffer[0])
-        console.log('newly added', buffer[1])
-        console.log('newly added', buffer[2])
-        console.log('newly added', buffer[3])
-        console.log('newly added', buffer[4])
-        console.log('newly added', buffer[5])
-        console.log('newly added', buffer[6])
-        console.log('newly added', buffer[7])
-        console.log('newly added', buffer[8])
+    }
+        console.log('_________________________________')
+        console.log('newly added', buffer[0]["timestamp"])
+        console.log('newly added', buffer[1]["timestamp"])
+        console.log('newly added', buffer[2]["timestamp"])
+        console.log('newly added', buffer[3]["timestamp"])
+        console.log('newly added', buffer[4]["timestamp"])
+        console.log('newly added', buffer[5]["timestamp"])
+        console.log('newly added', buffer[6]["timestamp"])
+        console.log('newly added', buffer[7]["timestamp"])
+        console.log('newly added', buffer[8]["timestamp"])
 }
 
 
@@ -77,7 +44,7 @@ function data_stream_update() {
 
 
 
-var data_streaming = setInterval(data_stream_update, 2000)
+var data_streaming = setInterval(data_stream_update, 1000)
 
 // For development
 setTimeout(() => {
--- src/mocked_datacenter/mock_new_data_generator.js
+++ src/mocked_datacenter/mock_new_data_generator.js
@@ -4,51 +4,62 @@ import calculate_time from './generate_timestamp';
 // The request would pass a number parameter representing how many seconds we want back
 // Something like
 // Request URL: https://our_server_address/status/details?cache_age=30
-const DATA = require('./DATA_buffer_real-time.json');
 
 
-// let current_time = new Date();
 
-// var cached_data = [];
+
+function NewData(timestamp, machines) {
+    this.timestamp = timestamp;
+    this.machines = machines;
+  }
+
+
+
+
 
 
 var current_time;
-
-
 function mock_new_data_generator(cache_age) {
     current_time = new Date();
 
     // console.log('should be old' , msToTime(false, 0))
     // console.log('should be new' , msToTime(true, 0))
 
-    if (DATA) {
-        var cached_data = [];
+    var new_object_array_by_seconds = [];
 
-        DATA.map((value, index) => {
-            if (index < cache_age) {
-                cached_data[index] = value;
-    
-                // Generate Timestamps
-                cached_data[index]["timestamp"] = calculate_time(current_time, index);
-                // Generate Machines
-                // Generate ExE Age
-                // Generate CPU usage
-                // Generate RAM usage
-            };
-        
-        });
-
-        return cached_data;
-        // console.log(cached_data)
-        
-    } else {
-        throw 'Initial server data returned undefined.'
+    for(let i = 0; i < cache_age; i++) {
+        var get_time = calculate_time(current_time, i);
+        // console.log(get_time)
+        var get_machines = [
+            {
+                "machine_name": "Pixel-Cruncher_V2",
+                "machine_ip": "0000:0000:0000:0000:0000:0000:0000:0001"
+            },
+            {
+                "machine_name": "Threadripper",
+                "machine_ip": "0000:0000:0000:0000:0000:0000:0000:0002",
+                "processes": [
+                    {
+                        "process_name": "steam_client.exe"
+                    },
+                    {
+                        "process_name": "spotify_client.exe",
+                        "display_name": "Spotify",
+                        "exe_age": 50,
+                        "usage_cpu": 5,
+                        "usage_memory_bytes": 654654654
+                    }
+                ]
+            }
+        ]
+        var newly_constructed = new NewData(get_time, get_machines)
+        new_object_array_by_seconds.push(newly_constructed)
     }
+    return new_object_array_by_seconds;
 }
 
 
 
-console.log(mock_new_data_generator(30)[0])
 
 
 
