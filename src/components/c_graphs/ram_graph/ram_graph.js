13047606d420d8adc83b47b7a3eb5e27470ad9f9
Cristian Pascu <30505787+andrei-pascu@users.noreply.github.com>
2020-04-05 15:49:42 +0100

    Update ram_graph.js

--- src/components/c_graphs/ram_graph/ram_graph.js
+++ src/components/c_graphs/ram_graph/ram_graph.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-// this.props.historyData
-// this contains the 20 item history
-
 export default class RamGraph extends Component {
     constructor(props) {
         super(props);
@@ -21,34 +18,30 @@ export default class RamGraph extends Component {
         var history = this.props.historyData;
         var selectedMachineHistory = [];
         this.total_memory_bytes = 0;
-
         for (let x = 0; x < history.length - 10; x++) {
             var processes = history[x + 10]['machines'][selected_machine]['processes'];
             var memory_usage_sum = 0;
             for (let j = 0; j < processes.length; j++) {
                 memory_usage_sum += processes[j]['memory_usage'];
             }
-            this.total_memory_bytes = (memory_usage_sum / 1000000000).toFixed(1) + 'GB';
             var transformUsageToPercent = parseInt((memory_usage_sum / history[x + 10]['machines'][selected_machine]['ram_max_size']) * 100);
-            // console.log(transformUsageToPercent)
             selectedMachineHistory.push(transformUsageToPercent);
-            // console.log(selectedMachineHistory)
         }
-
         return selectedMachineHistory;
     }
 
     generate_svg() {
         var selectedMachineHistory = this.getMemoryUsageHistory();
-        // console.log(selectedMachineHistory)
         var generated_svg_polyline_points = '';
         var point_x_axis = 0;
-        this.last_sum = parseInt(selectedMachineHistory[selectedMachineHistory.length - 21])
-        // console.log(this.last_sum)
+        var graph_new_entry = selectedMachineHistory.length - 21;
+        this.last_sum = parseInt(selectedMachineHistory[graph_new_entry]);
+        var selected_machine_gb = (this.props.historyData[graph_new_entry]['machines'][this.props.specificMachineIndex]['ram_max_size'] / 1000000000)
+        this.total_memory_bytes = ((this.last_sum * selected_machine_gb) / 100).toFixed(1);
+
         for (let x = selectedMachineHistory.length - 1; x >= 0; x--) {
             var point_y_axis_selectedMachineHistory = parseInt(this.svg_inner['height'] - (selectedMachineHistory[x] / 100) * this.svg_inner['height']);
-            // 
-            // console.log(this.svg_inner['height']-point_y_axis_selectedMachineHistory)
+
             generated_svg_polyline_points += ` ${point_x_axis},${point_y_axis_selectedMachineHistory} `;
             point_x_axis += parseInt(this.svg_inner['width'] / 19);
         }
@@ -63,13 +56,12 @@ export default class RamGraph extends Component {
         if (!this.props.specificMachineProcesses) {
             return (
                 <span className="ram_graph-container">
-                    TEMP_LAST_GENERATED_SECOND_Memoryusage:
                 </span>
             )
         } else {
             return (
                 <div className="ram_graph-container">
-                    <p>{this.total_memory_bytes + ' : ' + this.last_sum + '%'}</p>
+                    <p>{this.total_memory_bytes + 'GB : ' + this.last_sum + '%'}</p>
                     <svg viewBox={this.svg_inner_viewbox} className="ram_chart">
                         <polyline points={this.generate_svg()}>
                         </polyline>
